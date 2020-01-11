51e02c6174eb4e214e734a6f4605ea0b534944c8
TimothyLi0123 <59607582+TimothyLi0123@users.noreply.github.com>
2020-01-11 16:30:19 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Tianyi Li'
+__email__ = 'tianyil@mit.edu'
+__version__ = '1.0.0'
+__reference__ = 'Li, T., and Zhang, P. (2019). Self-falsifiable Hierarchical Detection of Overlapping Communities On Social 	Networks. arXiv preprint arXiv:1912.02903.'
